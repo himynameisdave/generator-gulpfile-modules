8479699ae80c8bac8a85c9e457fd7b31d0e2c814
dlunny <dlunny@featurecreeptechnology.com>
2015-07-26 00:06:00 -0400

    very getting ready for a super basic soft launch

--- app/index.js
+++ app/index.js
@@ -99,8 +99,28 @@ module.exports = yeoman.generators.Base.extend({
         })
       }
     }.bind(this));
-
+    //  actually set our dev deps in the pkg.json we're building
     pkg.devDependencies = devDeps;
+
+
+    //  build out the 'default', 'dev', and 'build' task lists
+    var mainTasks = {};
+
+    //  default tasks config
+      mainTasks.def = this.props.tasks.filter(function( task ){
+        return task === 'server' ? task : false;
+      })
+    //  dev tasks config
+      mainTasks.dev = this.props.tasks.filter(function( task ){
+        return task === 'compile-less' || task === 'compile-sass' ? task : false;
+      });
+
+    loggit(mainTasks);
+    //  set our high-level props to hold the task data too
+    this.props.mainTasks = mainTasks;
+    //  set our main tasks in the pkg.json file
+    //  TODO: is this actually needed? if we're using high-level props
+    pkg.gulpTasks = mainTasks;
 
     //  write our new package.json file
     loggit("Attempting to write directories your package.json file!", "green", "=");
@@ -128,7 +148,7 @@ module.exports = yeoman.generators.Base.extend({
 
     this.props.tasks.forEach(function( task, i ){
       this.copy( 'tasks/_'+task+'.js', 'gulp/'+task+'.js' );
-      // loggit(task);
+
       done();
     }.bind(this));
 
--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -1,7 +1,13 @@
 var gulp       = require('gulp'),
     requireDir = require('require-dir'),
-    tasks      = requireDir('./gulp/');
+    tasks      = requireDir('./gulp/'),
+    pkg        = require('./package.json');
 
 
+<% if( mainTasks.def.length > -1 ) { %>
+  gulp.task( 'default', [ <% mainTasks.def.forEach(function(task){ %>'<%- task %>'<% }) %> ]);
+<% } %>
 
-gulp.task( 'default', [ <% if( tasks.indexOf('server') > -1 ){ %>'server'<% }; %> ]);
+<% if( mainTasks.dev.length > -1 ) { %>
+  gulp.task( 'dev', [ <% mainTasks.dev.forEach(function(task){ %><%- task %><% }) %> ]);
+<% } %>
